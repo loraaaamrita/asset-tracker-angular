64f953bd5d0026a33bfd3e3b7b01d7a317c6822a
brockclayton <brock@bluerockapps.com>
2019-09-16 08:01:20 -0600

    bump

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to field-asset-portal!');
+    expect(page.getTitleText()).toEqual('Welcome to asset-tracker!');
   });
 
   afterEach(async () => {
